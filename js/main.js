57415303e3a27eedafbe219aced0643b02e09823
Alex Heritier <alex.heritier@gmail.com>
2014-10-28 21:44:50 -0700

    added a menu for the about tab

--- js/main.js
+++ js/main.js
@@ -5,10 +5,14 @@ requirejs.config({
 	shim: {
 		'bootstrap': {
 			deps: ['jquery']
+		},
+		'jquery_ui': {
+			deps: ['jquery']
 		}
 	},
 	paths: {
 		'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+		'jquery_ui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min',
 		'bootstrap': '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min',
 		'async': 'lib/async'
 	}
@@ -21,8 +25,23 @@ function(){
     return window.google.maps;
 });
 
-require(['jquery', 'maps', 'bootstrap'], function($, maps) {
+require(['jquery', 'maps', 'bootstrap', 'jquery_ui'], function($, maps) {
 	$(document).ready(function() {
 		var map = maps.init("map_canvas", {lat: 35.6895, lng: 139.6917});
+
+		$('a[href="#/about"]').click(function() {
+			var about_container = $('.about_container');
+			if (about_container.css('left') !== "0px") {
+				about_container.css({
+					'left': '0px',
+					'opacity': 1
+				});
+			} else {
+				about_container.css({
+					'left': '-500px',
+					'opacity': 0
+				});
+			}
+		});
 	});
 });
