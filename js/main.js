a2f36d18bdbc9a96e330691c1ce811021be33f05
Alex Heritier <alex.heritier@gmail.com>
2014-10-29 20:24:07 -0700

    clicking on a link now deactivates other active links

--- js/main.js
+++ js/main.js
@@ -27,21 +27,51 @@ function(){
 
 require(['jquery', 'maps', 'bootstrap', 'jquery_ui'], function($, maps) {
 	$(document).ready(function() {
-		var map = maps.init("map_canvas", {lat: 35.6895, lng: 139.6917});
+		var map,
+			getLinkListener,
+			about_link,
+			about_parents,
+			add_report_link,
+			add_report_parents;
 
-		$('a[href="#/about"]').click(function() {
-			var about_container = $('.about_container');
-			if (about_container.css('left') !== "0px") {
-				about_container.css({
-					'left': '0px',
-					'opacity': 1
-				});
-			} else {
-				about_container.css({
-					'left': '-500px',
-					'opacity': 0
+		map = maps.init("map_canvas", {lat: 35.6895, lng: 139.6917});
+		getLinkListener = function(container, link_parents) {
+			return function() {
+				// deactivate all other active links
+				if (!$('.about_container').is(container) && $('.about_container').css('left') === "0px") {
+					$('a[href="#/about"]').trigger('click');
+				}
+				if (!$('.add_report_container').is(container) && $('.add_report_container').css('left') === "0px") {
+					$('a[href="#/add/"]').trigger('click');
+				}
+
+				// if container is off screen
+				if (container.css('left') !== "0px") {
+					container.css({
+						'left': '0px',
+						'opacity': 1
+					});
+				} else {	// if container is showing
+					container.css({
+						'left': '-500px',
+						'opacity': 0
+					});
+				}
+				// make the parent links active
+				link_parents.forEach(function(parent) {
+					parent.toggleClass('active');
 				});
 			}
-		});
+		};
+
+		about_link = $('a[href="#/about"]');
+		about_parents = [];
+		about_parents.push(about_link.parent());
+		about_link.click(getLinkListener($('.about_container'), about_parents));
+
+		add_report_link = $('a[href="#/add/"]');
+		add_report_parents = [];
+		add_report_parents.push(add_report_link.parent(), add_report_link.parent().parent().parent());
+		add_report_link.click(getLinkListener($('.add_report_container'), add_report_parents));
 	});
 });
