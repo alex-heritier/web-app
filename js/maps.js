68e4a943c30b2be7d7f3e2cc0f1c80691f64add7
Alex Heritier <alex.heritier@gmail.com>
2014-11-07 15:00:47 -0800

    Made a sidebar module for sidebar code

--- js/maps.js
+++ js/maps.js
@@ -2,7 +2,7 @@
 
 define(['gmaps'], function(gmaps) {
     function init_map(id, coordinates) {
-        coordinates = coordinates || { lat: 37.761513, lng: -122.401830};
+        coordinates = coordinates || {lat: 37.761513, lng: -122.401830};
         var mapOptions = {
             center: coordinates,
             zoom: 10
