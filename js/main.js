ac7bc606796dbbd6b757f0dc49ccfaf796adeab0
Alex Heritier <alex.heritier@gmail.com>
2014-11-08 23:20:16 -0800

    Getting reports now works for local testing

--- js/main.js
+++ js/main.js
@@ -28,34 +28,28 @@ require(['jquery', 'maps', 'sidebars', 'reports', 'bootstrap'], function($, maps
 	$(document).ready(function() {
 		var map,
 			sidebars,
-			// report_data,
-			report_list;
+			report_data,
+			report_list,
+			result;
 
 		map = maps.init("map_canvas", {lat: 22.5500, lng: 114.1000});
 		sidebars = sb.init();
 
-		/*report_data = [
-			{
-				location: {
-					lat: 50,
-					lng: 90
-				}
-			},
-			{
-				location: {
-					lat: 10,
-					lng: 30
-				}
-			},
-		];*/
-		var result = $.get("server/web-app_server/get_report.php", function(data) {
-			data = $.parseJSON(data);
-			data.forEach(function(report) {
+		result = $.get("server/web-app_server/get_report.php", function(data) {
+			try {	// works online, fails locally
+				report_data = $.parseJSON(data);
+			}
+			catch (e) {
+				report_data = [
+					{location: {lat: 50, lng: 90}},
+					{location: {lat: 10, lng: 30}},
+				];
+			}
+			report_data.forEach(function(report) {
 				report.location.lat = parseFloat(report.location.lat);
 				report.location.lng = parseFloat(report.location.lng);
 			});
-			console.log(data);
-			report_list = reports.init(map, data);
+			report_list = reports.init(map, report_data);
 		});
 	});
 });
