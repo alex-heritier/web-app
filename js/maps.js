413b5b19051db06802403a42f158291ae274729d
Alex Heritier <alex.heritier@gmail.com>
2014-11-09 15:19:10 -0800

    Changed map zoom to 10

--- js/maps.js
+++ js/maps.js
@@ -5,7 +5,7 @@ define(['gmaps'], function(gmaps) {
         coordinates = coordinates || {lat: 37.761513, lng: -122.401830};
         var mapOptions = {
             center: coordinates,
-            zoom: 1,
+            zoom: 10,
             disableDefaultUI: true,
             zoomControl: true,
             zoomControlOptions: {
